f9ccd27fd4af33ac1892fb64b8f37ddb2e6ec01e
Ande <andeschoonover@gmail.com>
2021-02-26 19:46:18 -0800

    GSAP movement and Google Added This isn't near as much as I wanted. It was such a struggle to get this to actually work. I couldn't get the hair to animate and I'm not sure why.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
 console.log("OK");
 
-gsap.to;
+gsap
+  .timeline()
+  .to("#rightForearm", {
+    svgOrigin: "303px 277px",
+    rotation: 25,
+    duration: 1,
+    delay: 2
+  })
+  .to("#rightForearm", {
+    rotation: -35,
+    duration: 2
+  });
+
+gsap
+  .timeline({ repeat: -1 })
+  .to("#rightForearm", {
+    rotation: 25,
+    svgOrigin: "303px 277px",
+    duration: 0.5
+  })
+  .to("#rightForearm", {
+    rotation: -35,
+    duration: 0.75
+  })
+  .to("#rightForearm", {
+    rotation: 25,
+    duration: 0.5
+  });
